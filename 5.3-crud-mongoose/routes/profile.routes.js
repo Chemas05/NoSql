3cacb6188e3275594ac703140561d5195137cb56
Chemas05 <jsac_0805@hotmail.com>
2023-09-26 17:10:51 -0500

    taller de SQL

---
+++ 5.3-crud-mongoose/routes/profile.routes.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { getProfile } from "../controllers/profile.controller.js";
+import authJWT from '../middleware/auth.js';
+
+const profileRouter = Router();
+
+profileRouter.get('/me', authJWT, getProfile);
+
+export { profileRouter };
